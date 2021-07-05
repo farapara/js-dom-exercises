5bf037833838e9cd383cd954e5c92d9d28c6d723
Evi Fara <faraparaskevi@gmail.com>
2021-07-05 20:25:48 +0200

    Add solutions applying DOM selection methods, event listeners and DOM modifying methods

---
+++ 2-add-remove-class/solution.js
@@ -0,0 +1,5 @@
+const h1Element = document.querySelector("h1");
+const h2Element = document.querySelector("h2");
+
+h1Element.classList.add("super-header");
+h2Element.classList.remove("mini-header");
--- 8-form/solution.js
+++ 8-form/solution.js
@@ -1,1 +1,28 @@
 const data = [];
+const formElement = document.querySelector(".super-form");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(formElement);
+  const name = formElement.name.value;
+  const age = formElement.age.value;
+  const hobbies = formElement.hobbies.value;
+  const checked = formElement.married.checked;
+
+  console.log(name);
+  console.log(age);
+  console.log(hobbies);
+  console.log(checked);
+
+  const formData = {
+    name,
+    age,
+    hobbies,
+    checked,
+  };
+  data.push(formData);
+
+  console.log(data);
+
+  formElement.reset();
+});
